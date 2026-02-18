ae1ff68f55d998aebbe725136451e819669b7b54
Maureenlc89 <mleonc@ucenfotec.ac.cr>
2026-02-17 20:31:35 -0600

    Update build.gradle.kts

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,6 +17,8 @@ kotlin {
         implementation(projects.composeApp)
         implementation(libs.androidx.activity.compose)
         implementation(libs.compose.uiToolingPreview)
+        implementation(project.dependencies.platform(libs.koin.bom))
+        implementation(libs.koin.android)
     }
 }
 
