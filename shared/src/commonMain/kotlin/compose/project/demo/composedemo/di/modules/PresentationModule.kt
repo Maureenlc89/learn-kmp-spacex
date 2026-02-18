e5e9ee3d5df67e265df13d563410badef6fb9223
Maureen León Castro <mleonc06@gmail.com>
2026-02-17 21:44:46 -0600

    Laboratorio DI

---
+++ shared/src/commonMain/kotlin/compose/project/demo/composedemo/di/modules/PresentationModule.kt
@@ -0,0 +1,8 @@
+package compose.project.demo.composedemo.di.modules
+
+import org.koin.dsl.module
+
+
+val presentationModule = module {
+    // Define aquí las dependencias de tu capa de presentación
+}
